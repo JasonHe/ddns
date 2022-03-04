52afb611088bd69d45de70a22e4da8f3c1fea294
Jason He <59273498+JasonHe@users.noreply.github.com>
2022-03-04 16:05:14 +0000

    Create ddns.sh

---
+++ ddns.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir ./ddns
+cd ./ddns
+uname -m | grep -qi aarch64 && oarch=linux_arm64 || oarch=linux_x86_64;
+wget "$(curl -s https://api.github.com/repos/jeessy2/ddns-go/releases/latest|grep -i "browser_download_url.*${oarch}"|awk -F '"' '{print $(NF-1)}')";
+tar -xzvf ./ddns*.tar.gz
+chmod +x ./ddns-go
+mv ./ddns-go /usr/bin
+cd ..
+rm -rf ./ddns
+ddns-go -s install
